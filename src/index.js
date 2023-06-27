8c1147338470b6201f9d9537ab4b5c44bf5acf2f
Semeniuk Mykhailo <micanton0709@gmail.com>
2023-06-27 10:13:18 -0400

    .

--- src/index.js
+++ src/index.js
@@ -1,69 +1,49 @@
 import './css/styles.css';
-import { fetchCountries } from "./fetchCountries";
-const debounce = require('lodash.debounce');
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
+import Notiflix from 'notiflix';
 
-const DEBOUNCE_DELAY = 300;
+const loader = document.querySelector('.loader');
+const selectCat = document.querySelector('.breed-select');
+const catInfoEl = document.querySelector('.cat-info');
 
-const searchBoxEl = document.querySelector("input#search-box");
-const countryListEl = document.querySelector('ul.country-list');
-const countryInfoEl = document.querySelector('div.country-info');
+selectCat.onchange = showInfoCat;
+  
+function makeupSelect() { 
+    loader.classList.remove('hide');
+    fetchBreeds()
+        .then(data => {
+          loader.classList.add('hide');
+          selectCat.innerHTML = data.map(({ name, id }) => `<option value="${id}">${name}</option>`).join('');
+          selectCat.classList.remove('hide');
+      })
+        .catch(() => {
+            loader.classList.add('hide');
+            Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!');
+      });
+    
+}
+makeupSelect() 
 
-searchBoxEl.addEventListener('input', debounce(handleSearchBoxInput, DEBOUNCE_DELAY));
-
-let countryName = '';
-
-function handleSearchBoxInput(event) {
-    countryName = event.target.value.trim();
-
-    if (!countryName) {
-        clearInterface();
-        return;
-    };
-
-    fetchCountries(countryName)
-    .then(data => {
-        if (data.length === 1) {
-            clearInterface();
-            countryInfoEl.innerHTML = renderSingleMurkup(data);
-        } else if (data.length > 1 && data.length <= 10) {
-            clearInterface();
-            countryListEl.innerHTML = renderMultipleMurkup(data);            
-        } else {
-            clearInterface();
-            Notify.info("Too many matches found. Please enter a more specific name.");
-        }            
-     })    
-        .catch(error => {
-        clearInterface();
-        Notify.failure("Oops, there is no country with that name");
-    });
+function showInfoCat() {
+  catInfoEl.innerHTML = '';
+    loader.classList.remove('hide');
+    fetchCatByBreed(selectCat.value)
+      .then(data => {
+       loader.classList.add('hide');
+        makeUpCatInfo(data[0]);
+      })
+        .catch(() => {
+          loader.classList.add('hide');
+        Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!');
+      });
 }
 
-function renderSingleMurkup(data) {
-    const languages = Object.values(data[0].languages).join(', ');
-
-    return data.map(({ capital, flags, name, population }) => {
-        return `<div class="country-info__wrapper">
-                    <img src="${flags.svg}" alt="Country flag" width="40" height="30" class="country-info__flag" />
-                    <p class="country-info__name">${name.official}</p>
-                </div>
-                <p class="country-info__capital"><b>Capital:</b> ${capital}</p>
-                <p class="country-info__population"><b>Population:</b> ${population}</p>
-                <p class="languages"><b>Languages:</b> ${languages}</p>`
-    }).join('');  
-}
-
-function renderMultipleMurkup(data) {
-    return data.map(({ flags, name }) => {
-        return `<li class="country-list__item">
-                    <img src="${flags.svg}" alt="Conntry flag" width="40" height="30" class="country-list__flag">
-                    <p class="country-list__name">${name.official}</p>
-                </li>`;
-    }).join('');
-}
-
-function clearInterface() {
-    countryListEl.innerHTML = '';
-    countryInfoEl.innerHTML = '';
+function makeUpCatInfo(breed) {
+    loader.classList.add('hide');
+    let title = `<h2>${breed.breeds[0].name}</h2>`
+    let image = `<div><img src="${breed.url}"></div>`;
+    let description = `<h3>Description</h3><p>${breed.breeds[0].description}</p>`;
+    let temperament = `<h3>Temperament</h3><p>${breed.breeds[0].temperament}</p>`;
+    catInfoEl.innerHTML = image + '<div class="content">' + title + description + temperament + '</div>';
+    console.log(title);
 }
